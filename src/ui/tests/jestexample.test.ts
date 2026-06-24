aba04ef5a4df110bf28db805b0fe7ad318af6f94
Mouravi-N-E <nikolozi_mouravidze@epam.com>
2026-06-24 17:08:13 +0400

    small fix

--- src/ui/tests/jestexample.test.ts
+++ src/ui/tests/jestexample.test.ts
@@ -1,7 +1,7 @@
-// import { test, expect } from '@jest/globals';
-// import { sum } from './jestexample.js';
+import { test, expect } from '@jest/globals';
+import { sum } from './jestexample.js';
 
 
-// test('adds 1 + 2 to equal 3', () => {
-//   expect(sum(1, 2)).toBe(3);
-// });
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
