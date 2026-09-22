e22916f12d986c2c5a15803f7a8cd44d82628406
Mouravi-N-E <nikolozi_mouravidze@epam.com>
2026-09-22 21:30:23 +0400

    again

--- src/utils/global.setup.ts
+++ src/utils/global.setup.ts
@@ -2,6 +2,6 @@ import { test as setup } from '@playwright/test';
 import { getJWTToken } from './apiAuth';
 
 setup('getApiToken', async ({ request }) => {
-  const jwtToken = await getJWTToken(request);
+  const jwtToken = await getJWTToken(request, {isAdmin: true});
   process.env.JWT_TOKEN = jwtToken;
 }); 
