01b355b8ba097feaf477c89a353ca81b23161417
Mouravi-N-E <nikolozi_mouravidze@epam.com>
2026-09-23 17:46:30 +0400

    aaaaaaa

--- src/ui/tests/login-page.spec.ts
+++ src/ui/tests/login-page.spec.ts
@@ -4,12 +4,13 @@ test.describe('Login page Tests', () => {
   test.use({ storageState: { cookies: [], origins: [] } })
   test('Login with credentials happy path', {
     tag: '@Smoke'
-  }, async ({ loginPage, page }) => {
+  }, async ({ page }) => {
 
     // await expect(loginPage.page).toHaveTitle(/Report Portal/);
     await page.goto('https://playwright.dev/')
+    expect(page).toHaveTitle('boooo')
 
-    await loginPage.loginWithCredentials({ isAdmin: true });
+    //await loginPage.loginWithCredentials({ isAdmin: true });
   });
 
   test.skip('Socials links', {
